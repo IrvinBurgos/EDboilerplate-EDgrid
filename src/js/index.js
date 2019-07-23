e26b7b393d2c137fa5e202dd5c511e632a20f0e8
Irvin Burgos <irvinburgos8@gmail.com>
2019-07-23 02:02:27 -0600

    Commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import { openMenu } from './modules/open-menu'
+import { activeMenu } from './modules/active-menu'
+
+openMenu('main-menu', 'main-menu-toggle')
+activeMenu()
---
+++ src/js/modules/open-menu.js
@@ -0,0 +1,11 @@
+export const openMenu = (navId, toggleId) => {
+  const nav = document.getElementById(navId)
+  const toggle = document.getElementById(toggleId)
+
+  if (!nav || !toggle) return
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('show')
+    toggle.classList.toggle('active')
+  })
+}
